337f6a60e7359f2ab8c157b6f404953220af7c4b
Anuradha Srikanth <asrikant@andrew.cmu.edu>
2017-05-19 20:21:55 -0400

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+  $(document)
+  .ready(function() {
+
+      // fix menu when passed
+      $('.masthead')
+      .visibility({
+        once: false,
+        onBottomPassed: function() {
+          $('.fixed.menu').transition('fade in');
+        },
+        onBottomPassedReverse: function() {
+          $('.fixed.menu').transition('fade out');
+        }
+      })
+      ;
+
+      // create sidebar and attach to menu open
+      $('.ui.sidebar')
+      .sidebar('attach events', '.toc.item')
+      ;
+      // show modal
+      $('.ui.fullscreen.modal')
+      .modal({ blurring: true })
+      .modal('setting', 'transition', 'horizontal flip')
+      .modal('attach events', '.content.icon.menu','show')
+      ;
+
+    })
+  ;
+
+
+  /* Open when someone clicks on the span element */
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
