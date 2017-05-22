51ee7ba3f1e3eb2d63da73ff3ae320829e226067
Anuradha Srikanth <asrikant@andrew.cmu.edu>
2017-05-22 13:45:36 -0400

    demo added

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 
-  $(document)
-  .ready(function() {
+$(document)
+.ready(function() {
 
       // fix menu when passed
       $('.masthead')
@@ -30,16 +30,17 @@
 
 
     })
-  ;
+;
 
 
-  /* Open when someone clicks on the span element */
-  function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
 
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
 
