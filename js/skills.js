d69341f8c311426ea0ab046ffefeb9e145e82642
Anuradha Srikanth <asrikant@andrew.cmu.edu>
2017-06-18 22:54:18 -0400

    fixing scroll

--- js/skills.js
+++ js/skills.js
@@ -5,15 +5,65 @@ $(document)
 	// .sidebar('toggle')
 	// ;
 
+	// $('.ui.sticky.segment').hide();
 
-	$('#hide-button').click(function(){
-		$('.ui.segment.php').toggle();
+
+	// $('.row .column').click(function(){
+	// 	// $('.ui.sticky.segment.js').hide();
+
+	// 	var myId = $(this).attr("id");
+	// 	// alert('.' + myId);
+	// 	// console.log(.ui.right.very.close.rail);
+	// 	// $(".ui.right.very.close.rail .segment").each(function(e){
+	// 	$(".ui.right.very.close.rail .segment").each(function(i,e){
+	// 		console.log(myId);
+	// 		if($(e).hasClass(myId)){
+	// 			$(e).show();
+	// 		}
+	// 		// if($(e.className == myId)){
+	// 			// alert("blarg");
+	// 			// $(e).show();
+	// 		else{
+	// 			// alert(e.attr('class'));
+	// 			// e.preventDefault();
+	// 			$(e).hide();
+	// 			// e.preventDefault();
+	// 		}
+	// 	});
+	// 	// $('.ui.sticky.segment').hide();
+	// 	// $('.'+myId).show();
+
+	// });
+
+	$('.row .column').click(function(){
+		// $('.ui.sticky.segment.js').hide();
+
+		var myId = $(this).attr("id");
+		$(".ui.right.very.close.rail .segment .inside").each(function(i,e){
+ 			if(!($(e).hasClass(myId))){
+				// $(e).addClass('hide');
+				$(e).show();
+			}
+			else{
+				$(e).hide();
+				// $(e).removeClass('hide');
+			}
+		});
 	});
 
-	$('.ui.sticky')
-	.sticky({
-		context: '.ui.grid.sixteen.skill'
-	})
-	;
+// $('.row .column').click(function(){
+// 	$('.ui.right.very.close.rail').empty();
+// 	// $(".ui.right.very.close.rail .segment").each(function(i,e){
+
+// 	// }
+
+
+
+// }
+
+$('.ui.sticky')
+.sticky({
+	context: '.ui.grid.sixteen.skill'
+});
 
 });
